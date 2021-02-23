24af41c55bcd1acdab9171d31ad4332f16430027
Madison Schlosser <mcschlosser@miners.utep.edu>
2021-02-22 18:27:24 -0700

    Space_char, non_space_char and count_char complete

--- src/gui.c
+++ src/gui.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "tokenizer.h"
 #define LIMIT 100
 int main(){
   char arr[LIMIT];
@@ -11,4 +13,37 @@ int main(){
   printf("\n");
 
 }
+int space_char(char c){
+  if(c == ' ' || c == '\t' || c == '\n' && c != '\0'){
+    return 0;
+  }
+  return 1;
+}
+
+int non_space_char(char c){
+  if(c != ' ' || c != '\t' || c == '\n' && c != '\0'){
+    return 0;
+  }
+  return 1;
+}
+/*
+char *word_start(char *str){
+  
+}
+
+char *word_end(char *str){
+
+}
+int count_words(char *str){
+
+}
+*/
+int count_char(char *arr){
+  int count = 0;
+  for(int i = 0; *(arr + i) != '\0'; i++){
+    count++;
+  }
+  return count;
+}
+
 
